da3d306b6ec172282d5573d309ec406d547816c5
EnricoNipper <enrico.vicenzo@hotmail.com>
2024-02-12 09:03:33 -0300

    1.00

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,21 @@
+
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
-    <>
-    <div className="text-green-500">
-      Menu Principal 
-      </div>
-    </>
+    <div className="
+    bg-neutral-900
+    rounded-lg
+    h-full
+    w-full
+    overflow-hidden
+    overflow-y-auto
+    
+    ">
+      <Header>
+      header
+      </Header>
+    </div>
+    
   )
 }
